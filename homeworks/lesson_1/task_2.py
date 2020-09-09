c364f6004a2b10b03a47a01642d8a1e1025b8b1c
i1d <ivan.lamden@gmail.com>
2020-09-10 00:20:47 +0300

    commit task_2

--- homeworks/lesson_1/task_2.py
+++ homeworks/lesson_1/task_2.py
@@ -3,3 +3,7 @@
 и выведите в формате чч:мм:сс. Используйте форматирование строк.
 """
 _time = int(input("Enter time in seconds: "))
+_time_hours = _time // 3600
+_time_minutes = (_time - _time_hours * 3600) // 60
+_time_seconds = _time - _time_hours * 3600 - _time_minutes * 60
+print(f'Time: {_time_hours:>02}:{_time_minutes:>02}:{_time_seconds:>02}')
