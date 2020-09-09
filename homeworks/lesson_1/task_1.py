f15ea8909bfa3aeecc661d20475f8ed471c9bc4c
i1d <ivan.lamden@gmail.com>
2020-09-09 23:57:26 +0300

    commit task_1

---
+++ homeworks/lesson_1/task_1.py
@@ -0,0 +1,12 @@
+"""
+1. Поработайте с переменными, создайте несколько, выведите на экран,
+запросите у пользователя несколько чисел и строк и сохраните в переменные,
+выведите на экран.
+"""
+
+a = 1
+print(f'Variable a={a}')
+b = int(input('Input variable b (int): '))
+print(f'Variable b={b}')
+s = str(input("Input variable s (str): "))
+print(f'Variable s={s}')
