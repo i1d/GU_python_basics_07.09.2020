d37abf20145d4079468bb0f77edf68c2589e253f
i1d <ivan.lamden@gmail.com>
2020-09-10 10:04:59 +0300

    task_5 commit

--- homeworks/lesson_1/task_5.py
+++ homeworks/lesson_1/task_5.py
@@ -6,3 +6,16 @@
 Если фирма отработала с прибылью, вычислите рентабельность выручки (соотношение прибыли к выручке).
 Далее запросите численность сотрудников фирмы и определите прибыль фирмы в расчете на одного сотрудника.
 """
+revenue = int(input("Enter revenue: "))
+loss = int(input("Enter loss: "))
+if revenue > loss:
+    print("Profit! :)")
+    profitability = revenue / loss
+    employees_qty = int(input("Enter employees quantity: "))
+    print(f'Profitability factor: {profitability:.2f}')
+    print(f'Income by employee: {(revenue - loss) / employees_qty:.2f}')
+elif revenue == loss:
+    print("Zero :/")
+elif revenue < loss:
+    print("Loss :(")
+
