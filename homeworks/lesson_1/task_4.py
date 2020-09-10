f1fe6944003635e0b588a90f5f66e85aae386df9
i1d <ivan.lamden@gmail.com>
2020-09-10 09:54:12 +0300

    task_4 commit

--- homeworks/lesson_1/task_4.py
+++ homeworks/lesson_1/task_4.py
@@ -2,3 +2,11 @@
 4. Пользователь вводит целое положительное число. Найдите самую большую цифру в числе.
 Для решения используйте цикл while и арифметические операции.
 """
+val = int(input("Please enter positive integer value: "))
+_max = val % 10
+_val = val // 10
+while _val > 0:
+    if _val % 10 > _max:
+        _max = _val % 10
+    _val //= 10
+print(f'Max digit in the value: {_max}')
