5e0f6673fe86d2d7052376e41f62cedc32f50f4b
i1d <ivan.lamden@gmail.com>
2020-09-12 23:47:12 +0300

    lesson_2 task_2 commit

--- homeworks/lesson_2/l2_task2.py
+++ homeworks/lesson_2/l2_task2.py
@@ -4,3 +4,11 @@
 При нечетном количестве элементов последний сохранить на своем месте.
 Для заполнения списка элементов необходимо использовать функцию input().
 """
+_del = input("Enter the delimiter to split the list values:\n")
+_list = input(f"Enter list of values using the '{_del}' symbol as the delimiter:\n").split(_del)
+print(f'Initial list with size={len(_list)}: {_list}')
+for i in range(0, len(_list) - 1, 2):
+    tmp = _list[i]
+    _list[i] = _list[i + 1]
+    _list[i + 1] = tmp
+print(f'Converted list: {_list}')
