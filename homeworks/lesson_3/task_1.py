2059027c412dd2dd26600dbff9525e95e676b2da
i1d <ivan.lamden@gmail.com>
2020-09-15 23:19:03 +0300

    lesson_3 task_1 commit

--- homeworks/lesson_3/task_1.py
+++ homeworks/lesson_3/task_1.py
@@ -2,3 +2,12 @@
 1. Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление.
 Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
 """
+
+
+def div_func(a, b):
+#    a = float(input("Enter first value: "))
+#    b = float(input("Enter second value: "))
+    return a / b if b != 0 else float("inf")
+
+
+print(div_func(float(input("Enter first value: ")), float(input("Enter second value: "))))
