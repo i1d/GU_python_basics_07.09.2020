8ecd64b3d90bb5bfb9bcdca8b403c04ee8c62d8d
i1d <ivan.lamden@gmail.com>
2020-09-15 23:49:52 +0300

    lesson_3 task_2 commit

--- homeworks/lesson_3/task_2.py
+++ homeworks/lesson_3/task_2.py
@@ -4,3 +4,11 @@
 Функция должна принимать параметры как именованные аргументы.
 Реализовать вывод данных о пользователе одной строкой.
 """
+
+
+def enter_func(first_name="Ivan", last_name="Ivanov", year_of_birth=1900, city="Helsinki", email="ivan@ivanov.com", phone="+12345678900"):
+    return f'{first_name} {last_name} ({year_of_birth}): {city}, {email}, {phone}'
+
+
+print(enter_func(last_name='Fedorov', first_name='Petr', city="Tallinn", phone="+5134753634", year_of_birth="1950"))
+
