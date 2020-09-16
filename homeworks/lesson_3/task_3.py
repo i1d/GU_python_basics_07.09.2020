f4bb0e3a98f86a261a918734a74cd3223b5eed7f
i1d <ivan.lamden@gmail.com>
2020-09-16 21:52:48 +0300

    lesson_3 task_3 commit

--- homeworks/lesson_3/task_3.py
+++ homeworks/lesson_3/task_3.py
@@ -4,11 +4,8 @@
 """
 
 
-def my_func(a: float, b: float, c: float):
-    """Function to compute two biggest arguments (numbers!) and return their sum."""
-
-    try:
-        if
+def my_func(a, b, c):
+    """Function to compute two biggest arguments (of equal types) and return their sum."""
 
     if a > b and a > c:
         max = a
@@ -33,7 +30,30 @@ def my_func(a: float, b: float, c: float):
         max = mid = b
     if a == b == c:
         max = mid = a
+    if a == b < c:
+        max = c
+        mid = a
+    elif a == c < b:
+        max = b
+        mid = a
+    elif b == c < a:
+        max = a
+        mid = b
     return max + mid
 
 
-print(my_func("a", "b", 5))
+print(my_func(1, 1, 1))
+
+print(my_func(2, 1, 1))
+print(my_func(1, 2, 1))
+print(my_func(1, 1, 2))
+
+print(my_func(2, 2, 1))
+print(my_func(2, 1, 2))
+print(my_func(1, 2, 2))
+
+print(my_func(1, 2, 3))
+print(my_func(3, 1, 2))
+print(my_func(2, 3, 1))
+
+print(my_func("a", "b", "c"))
