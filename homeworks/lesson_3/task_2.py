c8d57c6c8802f3ebefddb06d6d8ae86be272d424
i1d <ivan.lamden@gmail.com>
2020-09-16 21:01:50 +0300

    lesson_3 task_2 commit

--- homeworks/lesson_3/task_2.py
+++ homeworks/lesson_3/task_2.py
@@ -7,8 +7,18 @@
 
 
 def enter_func(first_name="Ivan", last_name="Ivanov", year_of_birth=1900, city="Helsinki", email="ivan@ivanov.com", phone="+12345678900"):
+    """
+    Function to return set of named parameters of a user in a single row.
+    :param first_name:
+    :param last_name:
+    :param year_of_birth:
+    :param city:
+    :param email:
+    :param phone:
+    :return: str
+    """
     return f'{first_name} {last_name} ({year_of_birth}): {city}, {email}, {phone}'
 
 
-print(enter_func(last_name='Fedorov', first_name='Petr', city="Tallinn", phone="+5134753634", year_of_birth="1950"))
+print(enter_func(last_name='Fedorov', first_name='Petr', city="Tallinn", phone="+5134753634", year_of_birth=1950))
 
