bbf14a03ce95c194fbe50c322a1c18b8282b23aa
i1d <ivan.lamden@gmail.com>
2020-09-17 00:05:44 +0300

    lesson_3 task_6 commit

--- homeworks/lesson_3/task_6.py
+++ homeworks/lesson_3/task_6.py
@@ -11,4 +11,17 @@
 def int_func(_str):
     """Returns word with first capital letter"""
 
-    return _s
+    return _str[0].upper() + _str[1:]
+
+def int_func_2(_str):
+    """Returns word with first capital letter without using of upper() function."""
+
+    alphabet_lower = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+    alphabet_upper = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+    return alphabet_upper[alphabet_lower.index(_str[0])] + _str[1:] if _str[0] in alphabet_lower else _str
+
+
+print(int_func_2("text"))
+
+sentence = input("Enter set of words divided with 'space' symbol:\n").split(" ")
+print(" ".join([int_func(word) for word in sentence]))
