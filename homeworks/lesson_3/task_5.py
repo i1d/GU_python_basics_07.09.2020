8008d47750756c0759fcb27a4dbf81a5c347c873
i1d <ivan.lamden@gmail.com>
2020-09-16 23:08:50 +0300

    lesson_3 task_4 commit

--- homeworks/lesson_3/task_5.py
+++ homeworks/lesson_3/task_5.py
@@ -6,3 +6,25 @@
 Если специальный символ введен после нескольких чисел, то вначале нужно добавить сумму этих чисел
 к полученной ранее сумме и после этого завершить программу.
 """
+r = 0
+
+
+def list_sum(_list):
+    """Returns sum of items in a list."""
+
+    global r
+    for item in _list:
+        r += item
+    return r
+
+
+while True:
+    s = input("Enter set of numbers divided with 'space' symbol. Special character for stop: ").split(" ")
+    if not s[-1].isnumeric():
+        _s = [float(val) for val in s[:-1]]
+        print(list_sum(_s))
+        break
+    else:
+        _s = [float(val) for val in s]
+        print(list_sum(_s))
+
