1d47943a0c383db28573639b22df391a2fe40df9
i1d <ivan.lamden@gmail.com>
2020-09-21 13:25:14 +0300

    lesson_4 task_7 commit

--- homeworks/lesson_4/task_7.py
+++ homeworks/lesson_4/task_7.py
@@ -6,3 +6,23 @@
 а в цикле необходимо выводить только первые n чисел, начиная с 1! и до n!.
 Подсказка: факториал числа n — произведение чисел от 1 до n. Например, факториал четырёх 4! = 1 * 2 * 3 * 4 = 24.
 """
+from functools import reduce
+
+
+def fact(_n):
+    yield 1
+    k = 2
+    while k <= _n:
+        yield reduce(lambda a, b: a * b, range(1, k + 1))
+        k += 1
+
+
+n = input("Enter integer value: ")
+try:
+    n = int(n)
+    c = 1
+    for i in fact(n):
+        print(f'{c}! = {i}')
+        c += 1
+except ValueError:
+    print("Please input integer value.")
