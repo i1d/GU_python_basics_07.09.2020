412750b06fcd869a2d365ad571270cd1149ffec2
i1d <ivan.lamden@gmail.com>
2020-09-24 00:21:31 +0300

    lesson_5 task_5 commit

--- homeworks/lesson_5/task_5.py
+++ homeworks/lesson_5/task_5.py
@@ -1,2 +1,18 @@
 """5. Создать (программно) текстовый файл, записать в него программно набор чисел, разделенных пробелами.
 Программа должна подсчитывать сумму чисел в файле и выводить ее на экран."""
+import random
+
+random.seed()
+
+f_name = "task_5.txt"
+with open(f_name, "w") as f:
+    for _ in range(random.randint(10, 20)):
+        print(random.randint(0, 100), end=" ", file=f)
+with open(f_name, "r") as ff:
+    data = ff.read().rstrip(" ")
+    nums = data.split(" ")
+    s = 0
+    for n in nums:
+        s += int(n)
+    print(f'Nums: {", ".join(nums)}')
+    print(f'Sum = {s}')
