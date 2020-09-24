a56c9108ad02a63c34e9e3130002cd8398809123
i1d <ivan.lamden@gmail.com>
2020-09-24 14:51:08 +0300

    lesson_5 task_6 commit

--- homeworks/lesson_5/task_6.py
+++ homeworks/lesson_5/task_6.py
@@ -11,3 +11,36 @@
 
 Пример словаря:
 {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}"""
+
+
+def get_val(_str):
+    """converts "dirty" string to int"""
+
+    _s = ""
+    if _str == "—" or _str == "-":
+        _s = 0
+    else:
+        for s in _str:
+            if s.isnumeric():
+                _s += s
+    return int(_s)
+
+
+f_in = "task_6_in.txt"
+with open(f_in, "r") as f_in:
+    d = {}
+    h = 0
+    for num, row in enumerate(f_in, 1):
+        _r = []
+        if len(row) > 1:  # "empty string has \n symbol"
+            r = row.rstrip("\n").split(":")
+            _r.append(r[0])
+            r_ = r[1].lstrip(" ").rstrip(" ").split(" ")
+            for i in r_:
+                try:
+                    _r.append(get_val(i))
+                except ValueError as err:
+                    print(f'There are something wrong with row #{num}. Details: {err}.')
+            h = sum(_r[1:])
+            d[_r[0]] = h
+print(d)
