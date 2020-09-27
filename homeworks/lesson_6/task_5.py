28d0fe496ca1cc0c6c20d0a3d0462ba5c9b173fe
i1d <ivan.lamden@gmail.com>
2020-09-27 19:52:08 +0300

    lesson_6 task_5 commit

--- homeworks/lesson_6/task_5.py
+++ homeworks/lesson_6/task_5.py
@@ -3,3 +3,41 @@
 Pencil (карандаш), Handle (маркер). В каждом из классов реализовать переопределение метода draw.
 Для каждого из классов методы должен выводить уникальное сообщение. Создать экземпляры классов и проверить,
 что выведет описанный метод для каждого экземпляра."""
+
+
+class Stationery:
+    title = ""
+
+    def draw(self):
+        print("Запуск отрисовки.")
+
+
+class Pen(Stationery):
+    def draw(self):
+        Stationery.title = "Ручка"
+        print("Отрисовка ручкой.", end=" ")
+
+
+class Pencil(Stationery):
+    def draw(self):
+        Stationery.title = "Карандаш"
+        print("Отрисовка карандашом.", end=" ")
+
+
+class Handle(Stationery):
+    def draw(self):
+        Stationery.title = "Маркер"
+        print("Отрисовка маркером.", end=" ")
+
+
+pen = Pen()
+pen.draw()
+print(pen.title)
+
+pencil = Pencil()
+pencil.draw()
+print(pencil.title)
+
+handle = Handle()
+handle.draw()
+print(handle.title)
