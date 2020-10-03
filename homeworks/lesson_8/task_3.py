7125f7076915aa1eff4907a145b8e7bf9ec9ac4c
i1d <ivan.lamden@gmail.com>
2020-10-03 22:10:28 +0300

    lesson_8 task_3 commit

--- homeworks/lesson_8/task_3.py
+++ homeworks/lesson_8/task_3.py
@@ -8,3 +8,61 @@
 только если введено число. Класс-исключение должен не позволить пользователю ввести текст (не число) и отобразить
 соответствующее сообщение. При этом работа скрипта не должна завершаться.
 """
+int_digits = list(map(str, (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)))
+
+
+class NumberError(Exception):
+    def __init__(self, text):
+        self.text = text
+
+
+def is_int(str):
+    c = 0
+    for el in str:
+        if el in int_digits:
+            c += 1
+    if c == len(str):
+        return True
+    else:
+        return False
+
+
+def is_float(str):
+    c = 0
+    cd = 0
+    for el in str:
+        if el == ".":
+            cd += 1
+    if cd > 1 or (cd == 1 and len(str) == 1):
+        return False
+    else:
+        idx = str.find(".")
+        n_str = str[0:idx] + str[idx + 1:]
+        for el in n_str:  # нужно снова проверить каждый элемент, тк какой-то может оказаться не цифрой
+            if is_int(el):
+                c += 1
+        if c == len(n_str):
+            return True
+        else:
+            return False
+
+
+inp = ""
+lst = []
+while True:
+    inp = input("\nEnter a number to add it to the list: ")
+    try:
+        if is_int(inp):
+            lst.append(int(inp))
+            print(f"Our list: {lst}.")
+        elif is_float(inp):  # чтоб в массиве могли оказаться числа как int, так и float
+            lst.append(float(inp))
+            print(f"Our list: {lst}.")
+        elif inp.lower() == "stop":
+            print(f"Our final list: {lst}.\nThank you, bye.")
+            break
+        else:
+            raise NumberError(f"This is not a number. This is {type(inp)}.")
+    except NumberError as err:
+        print(err)
+        print(f"Our list: {lst}.")
