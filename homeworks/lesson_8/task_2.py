9294d01d74f96a710de4f86814e0c8b9e137e584
i1d <ivan.lamden@gmail.com>
2020-10-03 19:52:18 +0300

    lesson_8 task_2 commit

--- homeworks/lesson_8/task_2.py
+++ homeworks/lesson_8/task_2.py
@@ -2,3 +2,21 @@
 вводимых пользователем. При вводе пользователем нуля в качестве делителя программа должна корректно обработать
 эту ситуацию и не завершиться с ошибкой.
 """
+
+
+class MyError(Exception):
+    def __init__(self, text):
+        self.text = text
+
+
+try:
+    a = int(input("Enter dividend: "))
+    b = int(input("Enter divider: "))
+    if b == 0:
+        raise MyError("Divider is zero!")
+except MyError as err:
+    print(f'{err} The result is infinite.')
+except ValueError as err:
+    print(f"Not a number. Details: {err}")
+else:
+    print(f"{a}/{b}={a/b}")
